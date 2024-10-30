1aa3947d2b59e274a7da067fca40aac5db868cbb
Andrés Osorio <144381281+Nacho-024A@users.noreply.github.com>
2024-10-30 12:53:24 -0500

    Create Persona.java
    Clase Persona con Getters y Setters

---
+++ Getters_Setters/Persona.java
@@ -0,0 +1,31 @@
+public class Persona {
+    // Atributos privados
+    private String nombre;
+    private int edad;
+
+    // Constructor
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if (edad >= 0) {  // Validación para que la edad no sea negativa
+            this.edad = edad;
+        } else {
+            System.out.println("La edad no puede ser negativa.");
+        }
+    }
+}
