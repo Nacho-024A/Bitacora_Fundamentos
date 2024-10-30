b3dde875cfbbae33293f0583eb889ee1be0fa696
Andrés Osorio <144381281+Nacho-024A@users.noreply.github.com>
2024-10-30 11:56:30 -0500

    Create Main.java
    Cuerpo principal del codigo

---
+++ POO/Primera_Generacion/Main.java
@@ -0,0 +1,16 @@
+package primerageneracion;
+
+import segundageneracion.Dracula;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dracula obj = new Dracula("Otto", 143, 190);
+
+        obj.chuparsangre();
+        obj.morder();
+        obj.hipnotizar();
+        obj.aguantarsol();
+
+    }
+}
